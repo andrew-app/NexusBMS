bbfd1cb72ea21e609f4b03cd97bff61d5ff3a0fc
andrewapp <andrew.appuhamy@gmail.com>
2021-11-08 02:12:21 +1100

    lighting added

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
-import sys
-from nyamuk import *
+import os
+import string
+from art import *
 
-def nloop(client):
-    client.packet_write()     # flush write buffer (messages sent to MQTT server)
-    client.loop()             # fill read buffer   (enqueue received messages)
-    return client.pop_event() # return 1st received message (dequeued)
-def config():
-    client = Nyamuk("Nexus_BMS", port=1883)
-    ret = client.connect(version=3)
-    ret = nloop(client) # ret should be EventConnack object
-    if not isinstance(ret, EventConnack) or ret.ret_code != 0:
-        print("Connection Failed")
-        exit()
-    return client
+class nbms:
+    def __init__(self):
+        self.cmd = {"-l": "Lighting", "-hvac": "HVAC"}
+        self.rooms = string.ascii_uppercase[:20]
+    def getcmd(self, item):
+        try:
+            return self.cmd[item]
+        except KeyError:
+            print("Invalid type ", item)
+            return 0
 
-    
+    def getroom(self, id):
+        if len(str(id)) == 1 and id.upper() in self.rooms:
+            return id.upper()
+        else:
+            print("Invalid Room ID.", id)
+            return 0
 
-while(1):
-    client = config()
-    cmd = raw_input(">>")
-    cmd = str(cmd).split()
 
-    if "-l" in cmd and len(cmd) == 3:
-        topic = "Lighting/Room_"
-        if cmd[1].isdigit() is False:
-            topic += cmd[1].upper()
-            if cmd[2] is '0' or '1':
-                client.publish(topic, cmd[2], qos=0)
-                print("PUBLISH Sent")
-                client.disconnect()
-    
-    elif "-l" in cmd and len(cmd) == 2:
-        topic = "Lighting"  
-        if cmd[1] is '0' or '1':
-            client.publish(topic, cmd[1], qos=0)
-            print("PUBLISH Sent")
-            client.disconnect()
 
-    elif "-q" in cmd:
-        print("Shutting Down Nexus BMS")
-        break
-    else:
-        print("Invalid")
-    
-    
-    
-    
+if __name__ == "__main__":
+    tprint("NBMS")
+    print("Welcome to Nexus Building Managment System.")
+    n = nbms()
+    base_cmd = "mosquitto_pub -t"
+
+    while(1):
+        usrin = input(">>>")
+
+        if usrin == "-q":
+            print("Exiting...")
+            break
+
+        out = list(map(str,usrin.split()))
+
+        if len(out) == 3 and out[2] == '0' or out[2] == '1':
+
+            t1 = n.getcmd(out[0])
+            t2 = n.getroom(out[1])
+            mqtt = f"{t1}/Room_{t2} '{out[2]}'"
+            cmd = f"{base_cmd} {mqtt}"
+            
+            if t2 != 0:
+                print(cmd)
+                os.system(cmd)
+            
+
+        else:
+            print("Invalid command.")
